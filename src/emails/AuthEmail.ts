6d2b5c445d23f237013fa4e0b22c5049c4d8a913
Diego de Jesús Espinoza García <di3goDjesus@hotmail.com>
2025-04-28 18:34:52 -0700

    AuthEmail nuevo, brevoemail y AuthController modificados

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -1,4 +1,4 @@
-import { transporter } from "../config/nodemailer"
+import { sendEmail } from "../config/brevoemail";
 
 interface IEmail {
     email: string
@@ -7,37 +7,48 @@ interface IEmail {
 }
 
 export class AuthEmail {
-    static sendConfirmationEmail = async ( user : IEmail ) => {
-        const info = await transporter.sendMail({
-            from: 'UpTask <admin@uptask.com>',
-            to: user.email,
-            subject: 'UpTask - Confirma tu cuenta',
-            text: 'UpTask - Confirma tu cuenta',
-            html: `<p>Hola: ${user.name}, has creado tu cuenta en UpTask, ya casi esta todo listo, solo debes confirmar tu cuenta</p>
-                <p>Visita el siguiente enlace:</p>
-                <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
-                <p>E ingresa el código: <b>${user.token}</b></p>
-                <p>Este token expira en 10 minutos</p>
-            `
-        })
+    static sendConfirmationEmail = async (user: IEmail) => {
+        console.log('Enviando email de confirmación a:', user.email);
+        console.log('Token de confirmación:', user.token);
+        
 
-        console.log('Mensaje enviado', info.messageId)
-    }
+        const emailData = {
+             sender: { name: 'Agenda_Contactos', email: 'di3goDev.com' },
+             to: [{ email: user.email, name: user.name }],
+             subject: 'Agenda_Contactos - Confirma tu cuenta',
+             htmlContent: `
+                <html>
+                    <body>
+                        <p>Hola: ${user.name}, has creado tu cuenta en Agenda_Contactos, ya casi está todo listo, solo debes confirmar tu cuenta.</p>
+                        <p>Visita el siguiente enlace:</p>
+                        <a href="https://example.com/auth/confirm-account">Confirmar cuenta</a>
+                        <p>Ingresa el código: <b>${user.token}</b></p>
+                        <p>Este token expira en 10 minutos.</p>
+                    </body>
+                </html>
+            `,
+            replyTo: { email: 'admin@di3goDev.com', name: 'Di3goDev Support' },
+            headers: { 'X-Custom-Header': 'custom-value' },
+            params: { token: user.token, name: user.name },
+        }
 
-    static sendPasswordResetToken = async ( user : IEmail ) => {
-        const info = await transporter.sendMail({
-            from: 'UpTask <admin@uptask.com>',
-            to: user.email,
-            subject: 'UpTask - Reestablece tu password',
-            text: 'UpTask - Reestablece tu password',
-            html: `<p>Hola: ${user.name}, has solicitado reestablecer tu password.</p>
-                <p>Visita el siguiente enlace:</p>
-                <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer Password</a>
-                <p>E ingresa el código: <b>${user.token}</b></p>
-                <p>Este token expira en 10 minutos</p>
-            `
-        })
+        await sendEmail(emailData);
+    };
 
-        console.log('Mensaje enviado', info.messageId)
-    }
+    // static sendPasswordResetToken = async ( user : IEmail ) => {
+    //     const info = await transporter.sendMail({
+    //         from: 'UpTask <admin@uptask.com>',
+    //         to: user.email,
+    //         subject: 'UpTask - Reestablece tu password',
+    //         text: 'UpTask - Reestablece tu password',
+    //         html: `<p>Hola: ${user.name}, has solicitado reestablecer tu password.</p>
+    //             <p>Visita el siguiente enlace:</p>
+    //             <a href="${process.env.FRONTEND_URL}/auth/new-password">Reestablecer Password</a>
+    //             <p>E ingresa el código: <b>${user.token}</b></p>
+    //             <p>Este token expira en 10 minutos</p>
+    //         `
+    //     })
+
+    //     console.log('Mensaje enviado', info.messageId)
+    // }
 }
